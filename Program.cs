052138fd58800f5d3e216f08730fcc605b022477
Rodrigo Tiago <rodrigotiagocl@gmail.com>
2023-01-28 13:27:13 -0300

    Testando as classes implementadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("1111111", "8", "seras", 32);
+i.Ligar();
+i.InstalarAplicativo("bola");
+i.ReceberLigacao();
+
+Nokia n = new Nokia("1111111", "8", "seras", 32);
+n.Ligar();
+n.InstalarAplicativo("bola");
+n.ReceberLigacao();
