1674c2aa238ce55c89d3930e9c1e134be39ab09e
Rodrigo Tiago <rodrigotiagocl@gmail.com>
2023-01-28 13:22:40 -0300

    completando a Classe abstrata Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero;
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        // DONE: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+            // DONE: Passar os parâmetros do construtor para as propriedades
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
